c987c4282cf975d6c9301973eeacc316545d4e37
Adel Zarrouk <azarrouk00@gmail.com>
2017-10-16 15:12:05 +0200

    Añadida entidad Tablero

--- app/models/Tablero.java
+++ app/models/Tablero.java
@@ -2,21 +2,46 @@ package models;
 
 import models.Usuario;
 
-public class Tablero{
-  private Usuario usuario;
-  private String nombre;
+import javax.persistence.*;
 
-  public Tablero(Usuario usuario,String nombre){
-    this.usuario=usuario;
+@Entity
+public class Tablero{
+  @Id
+  @GeneratedValue(strategy=GenerationType.AUTO)
+  Long id;
+  private String nombre;
+  @ManyToOne
+  @JoinColumn(name="administradorId")
+  private Usuario administrador;
+
+  public Tablero() {}
+  public Tablero(Usuario administrador,String nombre){
+    this.administrador=administrador;
     this.nombre=nombre;
   }
 
-  public Usuario getAdministrador(){
-    return this.usuario;
+  public Long getId(){
+    return this.id;
+  }
+
+  public void setId(Long id){
+    this.id=id;
   }
 
   public String getNombre(){
     return this.nombre;
+  }
+
+  public void setNombre(String nombre){
+    this.nombre=nombre;
+  }
+
+  public Usuario getAdministrador(){
+    return this.administrador;
+  }
+
+  public void setAdministrador(Usuario administrador){
+    this.administrador=administrador;
   }
 
 }
