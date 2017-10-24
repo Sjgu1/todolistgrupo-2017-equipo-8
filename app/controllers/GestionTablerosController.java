2459c933e6ad8b8eab7da57cb539fbfaf21fc71b
Adel Zarrouk Bermudez <azarrouk00@gmail.com>
2017-10-24 15:17:30 +0200

    Crearcontrolador y vista detalle tablero

--- app/controllers/GestionTablerosController.java
+++ app/controllers/GestionTablerosController.java
@@ -14,12 +14,14 @@ import services.UsuarioService;
 import services.TableroService;
 import models.Usuario;
 import models.Tablero;
+import models.TableroRepository;
 import security.ActionAuthenticator;
 
 public class GestionTablerosController extends Controller {
   @Inject FormFactory formFactory;
   @Inject UsuarioService usuarioService;
   @Inject TableroService tableroService;
+  @Inject TableroRepository tableroRepository;
 
   // Comprobamos si hay alguien logeado con @Security.Authenticated(ActionAuthenticator.class)
   // https://alexgaribay.com/2014/06/15/authentication-in-play-framework-using-java/
@@ -48,7 +50,8 @@ public class GestionTablerosController extends Controller {
       return badRequest(formNuevoTablero.render(usuario, formFactory.form(Tablero.class), "Hay errores en el formulario"));
      }
      Tablero tablero = tableroForm.get();
-     tableroService.creaTablero(idUsuario, tablero.getNombre());
+     tablero=tableroService.creaTablero(idUsuario, tablero.getNombre());
+     Logger.debug("Creado tablero: "+tablero.getId());
      flash("aviso", "El tablero se ha grabado correctamente");
      return redirect(controllers.routes.GestionTablerosController.listaTableros(idUsuario));
     }
@@ -77,12 +80,26 @@ public class GestionTablerosController extends Controller {
     if (connectedUser != idUsuario) {
       return unauthorized("Lo siento, no estás autorizado");
     } else {
-      Logger.debug("tablero:"+idTablero);
-      Logger.debug("usuari:"+idUsuario);
       tableroService.addParticipanteaTablero(idTablero,idUsuario);
       flash("aviso", "Se ha unido al tablero con éxito");
       //return redirect(controllers.routes.GestionTablerosController.listaTableros(idUsuario));
       return ok();
+    }
+  }
+
+  @Security.Authenticated(ActionAuthenticator.class)
+  public Result detalleTablero(Long idTablero, Long idUsuario){
+    String connectedUserStr = session("connected");
+    Long connectedUser =  Long.valueOf(connectedUserStr);
+    if (connectedUser != idUsuario) {
+      return unauthorized("Lo siento, no estás autorizado");
+    } else {
+      Tablero tablero = tableroRepository.findById(idTablero);
+      if (tablero == null) {
+        return notFound("Tablero no encontrado");
+      } else {
+        return ok(detalleTablero.render(tablero,idUsuario));
+      }
     }
   }
 }
