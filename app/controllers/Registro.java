c2fc42bdd1c6a0cedd67773dccf47b034e810b47
Sergio Julio Garcia Urdiales <sjgu1@alu.ua.es>
2017-11-08 14:03:08 +0100

    Probando repositorio

--- app/controllers/Registro.java
+++ app/controllers/Registro.java
@@ -2,6 +2,7 @@ package controllers;
 
 import play.data.validation.Constraints;
 
+//
 //Usamos los constraints para que se validen automáticamente
 //en el formulario: https://www.playframework.com/documentation/2.5.x/JavaForms
 public class Registro{
