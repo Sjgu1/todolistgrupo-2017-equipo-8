611af8127ec7072e5a211be67e04585f931c7d0e
Adel Zarrouk Bermudez <azarrouk00@gmail.com>
2017-11-19 19:07:15 +0100

    Adaptar controllers y vistas de tareas para tener en cuenta el campo fecha límite en listas, nuevas y modificar tareas

--- app/controllers/GestionTareasController.java
+++ app/controllers/GestionTareasController.java
@@ -13,6 +13,7 @@ import java.util.List;
 
 import services.UsuarioService;
 import services.TareaService;
+import services.TareaServiceException;
 import models.Usuario;
 import models.Tarea;
 import security.ActionAuthenticator;
@@ -33,7 +34,7 @@ public class GestionTareasController extends Controller{
       return unauthorized("Lo siento, no estás autorizado");
     } else {
       Usuario usuario = usuarioService.findUsuarioPorId(idUsuario);
-      return ok(formNuevaTarea.render(usuario, formFactory.form(Tarea.class),""));
+      return ok(formNuevaTarea.render(usuario, formFactory.form(Tareas.class),""));
     }
   }
 
@@ -41,16 +42,27 @@ public class GestionTareasController extends Controller{
   public Result creaNuevaTarea(Long idUsuario) {
     String connectedUserStr = session("connected");
     Long connectedUser =  Long.valueOf(connectedUserStr);
+    Usuario usuario;
     if ((long)connectedUser != (long)idUsuario) {
       return unauthorized("Lo siento, no estás autorizado");
     } else {
-      Form<Tarea> tareaForm = formFactory.form(Tarea.class).bindFromRequest();
+      Form<Tareas> tareaForm = formFactory.form(Tareas.class).bindFromRequest();
       if (tareaForm.hasErrors()) {
-        Usuario usuario = usuarioService.findUsuarioPorId(idUsuario);
-        return badRequest(formNuevaTarea.render(usuario, formFactory.form(Tarea.class), "Hay errores en el formulario"));
+        usuario = usuarioService.findUsuarioPorId(idUsuario);
+        return badRequest(formNuevaTarea.render(usuario, formFactory.form(Tareas.class), "Hay errores en el formulario"));
       }
-      Tarea tarea = tareaForm.get();
-      tareaService.nuevaTarea(idUsuario, tarea.getTitulo());
+      Tareas tarea = tareaForm.get();
+      tarea.fechaLimite=tarea.fechaLimite.equals("") ? null : tarea.fechaLimite;
+      if (tarea.fechaLimite==null)
+        tareaService.nuevaTarea(idUsuario, tarea.titulo,null);
+      else {
+        try {
+          tareaService.nuevaTarea(idUsuario, tarea.titulo,tarea.fechaLimite);
+        } catch (TareaServiceException e){
+          usuario = usuarioService.findUsuarioPorId(idUsuario);
+          return badRequest(formNuevaTarea.render(usuario, formFactory.form(Tareas.class), e.getMessage()));
+        }
+      }
       flash("aviso", "La tarea se ha grabado correctamente");
       return redirect(controllers.routes.GestionTareasController.listaTareas(idUsuario));
     }
@@ -84,8 +96,7 @@ public class GestionTareasController extends Controller{
         return unauthorized("Lo siento, no estás autorizado");
       } else {
         return ok(formModificacionTarea.render(tarea.getUsuario().getId(),
-        tarea.getId(),
-        tarea.getTitulo(),
+        tarea,
         ""));
       }
     }
@@ -95,7 +106,19 @@ public class GestionTareasController extends Controller{
   public Result grabaTareaModificada(Long idTarea) {
     DynamicForm requestData = formFactory.form().bindFromRequest();
     String nuevoTitulo = requestData.get("titulo");
-    Tarea tarea = tareaService.modificaTarea(idTarea, nuevoTitulo);
+    String nuevaFechaLimite = requestData.get("fechaLimite");
+    Tarea tarea;
+    Logger.info("fecha límite: "+nuevaFechaLimite);
+    if (nuevaFechaLimite.equals(""))
+      tarea=tareaService.modificaTarea(idTarea,nuevoTitulo,"01-01-1900");
+    else {
+      try {
+        tarea=tareaService.modificaTarea(idTarea, nuevoTitulo,nuevaFechaLimite);
+      } catch (TareaServiceException e){
+        tarea = tareaService.obtenerTarea(idTarea);
+        return badRequest(formModificacionTarea.render(tarea.getUsuario().getId(), tarea, e.getMessage()));
+      }
+    }
     return redirect(controllers.routes.GestionTareasController.listaTareas(tarea.getUsuario().getId()));
   }
 
