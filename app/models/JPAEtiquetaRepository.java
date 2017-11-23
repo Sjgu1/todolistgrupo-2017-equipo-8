d2f3453962e49b8d5dda4b1d4dd123413de47926
Adel Zarrouk Bermudez <azarrouk00@gmail.com>
2017-11-23 17:33:09 +0100

    Añadir repository etiquetas,datos por defecto a dataset xml y algun test nuevo

---
+++ app/models/JPAEtiquetaRepository.java
@@ -0,0 +1,49 @@
+package models;
+
+import javax.inject.Inject;
+import play.db.jpa.JPAApi;
+
+import java.util.List;
+
+import javax.persistence.EntityManager;
+
+public class JPAEtiquetaRepository implements EtiquetaRepository{
+  JPAApi jpaApi;
+
+  @Inject
+  public JPAEtiquetaRepository(JPAApi api){
+    this.jpaApi=api;
+  }
+
+  public Etiqueta add(Etiqueta etiqueta){
+    return jpaApi.withTransaction(entityManager -> {
+      entityManager.persist(etiqueta);
+      entityManager.flush();
+      entityManager.refresh(etiqueta);
+      return etiqueta;
+    });
+  }
+
+  public Etiqueta update(Etiqueta etiqueta){
+    return jpaApi.withTransaction(entityManager -> {
+      Etiqueta etiquetaBD=entityManager.find(Etiqueta.class,etiqueta.getId());
+      etiquetaBD.setNombre(etiqueta.getNombre());
+      etiquetaBD.setColor(etiqueta.getColor());
+      return etiquetaBD;
+    });
+  }
+
+  public void delete(Long idEtiqueta){
+    jpaApi.withTransaction(() -> {
+      EntityManager entityManager=jpaApi.em();
+      Etiqueta etiquetaBD=entityManager.getReference(Etiqueta.class,idEtiqueta);
+      entityManager.remove(etiquetaBD);
+    });
+  }
+
+  public Etiqueta findById(Long idEtiqueta){
+    return jpaApi.withTransaction(entityManager -> {
+      return entityManager.find(Etiqueta.class,idEtiqueta);
+    });
+  }
+}
--- test/models/EtiquetaTest.java
+++ test/models/EtiquetaTest.java
@@ -1,13 +1,60 @@
 import org.junit.*;
 import static org.junit.Assert.*;
 
+import play.db.Database;
+import play.db.Databases;
+import play.db.jpa.*;
+
 import java.lang.IllegalArgumentException;
 
 import play.Logger;
 
+import java.sql.*;
+
+import org.dbunit.*;
+import org.dbunit.dataset.*;
+import org.dbunit.dataset.xml.*;
+import org.dbunit.operation.*;
+import java.io.FileInputStream;
+
+import play.inject.guice.GuiceApplicationBuilder;
+import play.inject.Injector;
+import play.inject.guice.GuiceInjectorBuilder;
+import play.Environment;
+
 import models.Etiqueta;
+import models.EtiquetaRepository;
+import models.JPAEtiquetaRepository;
 
 public class EtiquetaTest {
+
+  static Database db;
+  static private Injector injector;
+
+  //Se ejecuta sólo una vez, al principio de todos los tests
+  @BeforeClass
+  static public void initApplication(){
+    GuiceApplicationBuilder guiceApplicationBuilder =
+      new GuiceApplicationBuilder().in(Environment.simple());
+    injector=guiceApplicationBuilder.injector();
+    db=injector.instanceOf(Database.class);
+    //Necesario para inicializar JPA
+    injector.instanceOf(JPAApi.class);
+  }
+
+  @Before
+  public void initData() throws Exception {
+    JndiDatabaseTester databaseTester = new JndiDatabaseTester("DBTest");
+    IDataSet initialDataSet = new FlatXmlDataSetBuilder().build(new FileInputStream("test/resources/usuarios_dataset.xml"));
+    databaseTester.setDataSet(initialDataSet);
+    databaseTester.setSetUpOperation(DatabaseOperation.CLEAN_INSERT);
+    databaseTester.onSetup();
+  }
+
+  private EtiquetaRepository newEtiquetaRepository(){
+    return injector.instanceOf(EtiquetaRepository.class);
+  }
+
 
   @Test
   public void testCrearEtiquetaColor(){
@@ -61,6 +108,14 @@ public class EtiquetaTest {
     String color2="ffffff";
     Etiqueta etiqueta =new Etiqueta(color);
     etiqueta.setColor(color2);
+  }
+
+  @Test
+  public void testCompruebaColor(){
+    Boolean correcto=Etiqueta.compruebaColor("#d93f0b");
+    Boolean incorrecto=Etiqueta.compruebaColor("jd93f0b");
+    assertTrue(correcto);
+    assertTrue(!incorrecto);
   }
 
 }
